3d52d1e7ea7bdd0a3b8d622d2311e2a525c974f7
Kian Candelario <kiancandelario1@gmail.com>
2024-05-08 19:22:04 +0800

    feat: Completed design for sign up page

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -11,14 +11,31 @@
 <body>
     <main class="relative w-screen h-screen">
         <div class="w-full h-full bg-gradient-to-br from-[#191E29] to-[#696E79] flex justify-center items-center">
-            <div class="w-[30%] h-[80%] bg-[#132D46] rounded-2xl p-5">
-                <div class="flex justify-center items-center">
-                    <img class="h-36 w-36" src="{{ asset('storage/logo.png') }}" alt="Logo">
-                    <div>
-                        <h1 class="font-bold text-[2.4rem]">Sign up</h1>
-                        <h3>Create an account</h3>
+            <div class="w-[22%] h-[78%] bg-[#132D46] rounded-2xl p-7 shadow-xl">
+                <div class="flex justify-center items-center mb-3">
+                    <img class="h-36 w-28" src="{{ asset('storage/logo.png') }}" alt="Logo">
+                    <div class="ml-6 text-white">
+                        <h1 class="font-bold text-4xl font-poppins mb-1">Sign up</h1>
+                        <h3 class="font-quicksand text-md opacity-65">Create an account</h3>
                     </div>
                 </div>
+                <form class="w-full flex flex-col justify-center items-center" action="" method="">
+                    <div class="w-full flex flex-col">
+                        <label class="ml-1 mb-1 text-white font-poppins text-xs" for="name">Username</label>
+                        <input class="mb-3 py-3 px-4 text-xs font-quicksand rounded-md" id="name" name="name" type="text" placeholder="Enter username">
+
+                        <label class="ml-1 mb-1 text-white font-poppins text-xs" for="email">Email</label>
+                        <input class="mb-3 py-3 px-4 text-xs font-quicksand rounded-md" id="email" name="email" type="text" placeholder="Enter email">
+
+                        <label class="ml-1 mb-1 text-white font-poppins text-xs" for="password">Password</label>
+                        <input class="mb-3 py-3 px-4 text-xs font-quicksand rounded-md" id="password" name="password" type="password" placeholder="Enter password">
+
+                        <label class="ml-1 mb-1 text-white font-poppins text-xs" for="password_confirmation">Confirm Password</label>
+                        <input class="mb-3 py-3 px-4 text-xs font-quicksand rounded-md" id="password_confirmation" name="password_confirmation" type="password" placeholder="Confirm your password">
+                    </div>
+
+                    <input class="w-[45%] h-9 font-bold bg-[#01C38D] font-poppins text-xs cursor-pointer mt-4 rounded-md text-[#F0F5F9] hover:bg-[#FFFFFF] hover:text-[#191E29] hover:scale-100 hover:translate-y-1 transition duration-300" type="submit" value="SIGN UP">
+                </form>
             </div>
         </div>
     </main>
