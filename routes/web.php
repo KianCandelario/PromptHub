0c5c0a7e07014e71d8438cfb393d461b37894632
Kian Candelario <kiancandelario1@gmail.com>
2024-05-19 00:29:00 +0800

    feat: Added a route to login page

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get('register', [UserController::class, 'signup']);
+Route::get('login', [UserController::class, 'signin']);
