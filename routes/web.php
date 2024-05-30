d1ea2c5890f725d71eb2b34f0324ad582fd7b176
Kian Candelario <kiancandelario1@gmail.com>
2024-05-31 00:46:21 +0800

    feat: Added new routes for new functionalities

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\CheckJustRegistered;
 
 
 Route::get('/', [UserController::class, 'landing_page']);
-Route::get('/home', [UserController::class, 'home_page']);
+Route::get('/home', [UserController::class, 'home_page'])->middleware('auth');
 
+
+// Register
+Route::get('/registered', [UserController::class, 'registered']);
 Route::get('register', [UserController::class, 'signup']);
-
 Route::post('/store', [UserController::class, 'store']);
 
+// Log in
 Route::get('login', [UserController::class, 'signin']);
 Route::post('/login', [UserController::class, 'authenticate']);
+
+// Log out
+Route::post('/logout', [UserController::class, 'logout']);
