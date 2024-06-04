c27f4fec8cef2f30dc9310b315897d8b612a3807
Kian Candelario <kiancandelario1@gmail.com>
2024-06-05 00:25:19 +0800

    feat: Added controller function for creating posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -10,7 +11,33 @@ class PostController extends Controller
         return view('templates.createpost');
     }
 
-    public function createPost() {
-        
+    public function createPost(Request $request) {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'body' => 'required',
+            'image' => 'nullable|mimes:png,jpg, jpeg, webp'
+        ]);
+
+        if ($request->has('image')) {
+            $file = $request->file('image');
+            $extension = $file->getClientOriginalExtension();
+
+            $filename = time().'.'.$extension;
+
+            $path = 'uploads/posts';
+            $file->move(public_path($path), $filename);
+            $incomingFields['image'] = $path.$filename;
+        } else {
+            $incomingFields['image'] = null;
+        }
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['description'] = strip_tags($incomingFields['description']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $incomingFields['user_id'] = auth()->id();
+        Post::create($incomingFields);
+
+        return redirect()->intended('/create-post');
     }
 }
