7d3c2f228ca50966b0f642edde4b4ae50a9b5b88
Kian Candelario <kiancandelario1@gmail.com>
2024-06-07 00:11:16 +0800

    style: Modified the style of the cards

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,8 +1,8 @@
-<div class="max-w-sm w-full h-[50%] bg-[#122840] border border-neon-green rounded-lg shadow-lg shadow-slate-500 hover:scale-110 hover:shadow-lg hover:shadow-neon-green transition">
+<div class="max-w-sm w-full h-[70%] bg-[#122840] border border-neon-green rounded-lg shadow-lg shadow-slate-500 hover:scale-90 hover:shadow-lg hover:shadow-neon-green transition">
     <img class="object-fill rounded-t-lg w-full h-[50%]" src="{{ $image }}" alt="" />
     <div class="h-[50%] p-5">
-        <h5 class="text-2xl font-bold tracking-tight text-white font-poppins" style="max-height: 3rem; overflow: hidden; text-overflow: ellipsis;">{{ $title }}</h5>
-        <p class="font-normal text-gray-400 font-quicksand" style="max-height: 1.5rem; overflow: hidden; text-overflow: ellipsis;">{{ $description }}</p>
+        <h5 class="text-2xl font-bold tracking-tight text-white font-poppins h-[3rem] truncate">{{ $title }}</h5>
+        <p class="font-normal text-gray-400 font-quicksand h-[1.5rem] truncate">{{ $description }}</p>
         <div class="flex items-center mt-4 h-[50%]">
             <img class="w-10 h-10 rounded-full mr-4" src="{{ asset('storage/codicon_account.svg') }}" alt="Avatar">
             <div class="text-sm text-neon-green font-quicksand">
