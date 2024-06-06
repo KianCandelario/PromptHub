afb6b2fd8887726a2dd775104cb2e46f0b72128e
Kian Candelario <kiancandelario1@gmail.com>
2024-06-06 23:34:41 +0800

    finished: Finished signed in and signed out layouts

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,34 +7,29 @@
 
         <title>PromptHub</title>
     </head>
-    <body>
-        <div class="w-screen h-screen bg-dark-grey text-white">
+    <body class="bg-dark-grey no-scrollbar overflow-x-hidden">
+        <div class="w-screen h-screen bg-dark-grey text-white no-scrollbar">
             <header class="flex justify-between items-center h-[11%] w-full px-20">
                 <a href="" class="font-poppins font-extrabold text-neon-green text-xl">Prompt<span class="text-white">Hub</span></a>
 
                 <div>
                     <ul class="flex gap-11 font-poppins font-medium text-sm">
-                        <li><a class="relative w-fit block after:block after:content-[''] after:absolute after:h-[1.5px] after:bg-neon-green after:w-full after:scale-x-0 after:hover:scale-x-100 hover:text-neon-green after:transition after:duration-300 after:origin-left" href="{{ route('home') }}">Home</a></li>
-                        <li><a class="relative w-fit block after:block after:content-[''] after:absolute after:h-[1.5px] after:bg-neon-green after:w-full after:scale-x-0 after:hover:scale-x-100 hover:text-neon-green  after:transition after:duration-300 after:origin-center" href="">Your Posts</a></li>
-                        <li><a class="relative w-fit block after:block after:content-[''] after:absolute after:h-[1.5px] after:bg-neon-green after:w-full after:scale-x-0 after:hover:scale-x-100 hover:text-neon-green  after:transition after:duration-300 after:origin-right" href="{{ route('create-post') }}">Create Post</a></li>
+                        <li><a class="relative w-fit block after:block after:content-[''] after:absolute after:h-[1.5px] after:bg-neon-green after:w-full after:scale-x-0 after:hover:scale-x-100 hover:text-neon-green after:transition after:duration-300 after:origin-left {{ request()->routeIs('home') ? 'active' : '' }}" href="{{ route('home') }}">Home</a></li>
+                        <li><a class="relative w-fit block after:block after:content-[''] after:absolute after:h-[1.5px] after:bg-neon-green after:w-full after:scale-x-0 after:hover:scale-x-100 hover:text-neon-green  after:transition after:duration-300 after:origin-center {{ request()->routeIs('posts', 'posts.*') ? 'active' : '' }}" href="{{ route('posts') }}">Your Posts</a></li>
+                        <li><a class="relative w-fit block after:block after:content-[''] after:absolute after:h-[1.5px] after:bg-neon-green after:w-full after:scale-x-0 after:hover:scale-x-100 hover:text-neon-green  after:transition after:duration-300 after:origin-center {{ request()->routeIs('create-post') ? 'active' : '' }}" href="{{ route('create-post') }}">Create Post</a></li>
+                        <li><a class="relative w-fit block after:block after:content-[''] after:absolute after:h-[1.5px] after:bg-neon-green after:w-full after:scale-x-0 after:hover:scale-x-100 hover:text-neon-green  after:transition after:duration-300 after:origin-right {{ request()->routeIs('about') ? 'active' : '' }}" href="{{ route('about') }}">About</a></li>
                     </ul>
                 </div>
 
                 <div class="flex items-center mt-3">
-                    <form class="flex items-center h-8" action="">
-                        <input class="h-full text-xs font-quicksand p-2 px-3 bg-transparent border-2 border-neon-green border-r-0 text-white rounded-tl-xl rounded-bl-xl" type="text" placeholder="Search...">
-                        <button class="flex justify-center items-center h-full border-neon-green border-2 p-2 px-3 border-l-0 rounded-tr-xl rounded-br-xl" type="submit">
-                            <img class="h-5 w-5" src="{{ asset('storage/heroicons_magnifying-glass-20-solid.svg') }}" alt="Search">
-                        </button>
-                    </form>
-
+                    
                     <form action="/logout" method="POST">
                         @csrf
                         <input class="ml-5 bg-neon-green font-quicksand px-4 py-2 text-white text-xs font-bold rounded-md cursor-pointer hover:bg-rose-500 transition duration-300" type="submit" value="Log out">
                     </form>
                 </div>
             </header>
-            <main class="h-[89%] w-full flex">
+            <main class="h-[89%] w-full no-scrollbar">
                 @yield('content')
             </main>
         </div>
