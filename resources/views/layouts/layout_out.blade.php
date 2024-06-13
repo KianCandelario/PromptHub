ebf5cdfe9215b521057846d7d2bb8c2359ae656b
Kian Candelario <kiancandelario1@gmail.com>
2024-06-13 23:13:28 +0800

    route: Routed the logo to landing page

--- resources/views/layouts/layout_out.blade.php
+++ resources/views/layouts/layout_out.blade.php
@@ -10,7 +10,7 @@
     <body class="bg-dark-grey no-scrollbar overflow-x-hidden">
         <div class="w-screen h-screen bg-dark-grey text-white no-scrollbar">
             <header class="flex justify-between items-center h-[11%] w-full px-20">
-                <a href="" class="font-poppins font-extrabold text-neon-green text-xl">Prompt<span class="text-white">Hub</span></a>
+                <a href="/" class="font-poppins font-extrabold text-neon-green text-xl">Prompt<span class="text-white">Hub</span></a>
 
                 <div class="flex justify-center items-center font-poppins">
                     <a class="mr-5 text-xs border-neon-green border-2 bg-neon-green px-5 py-2 rounded-md hover:bg-dark-blue transition-all" href="{{url('/login')}}">Login</a>
